61ed7dee78da804f255cc44850ea3a5e354aeafb
Sam Imhoff <simhoffe@gmail.com>
2023-02-01 13:10:15 -0500

    Added beginning of solution to levelOrder

--- binaryTreeTraverse.js
+++ binaryTreeTraverse.js
@@ -23,3 +23,36 @@ var inorderTraversal = function(root) {
   recurse(root);
   return results;
 };
+
+var postorderTraversal = function(root) {
+  const results = [];
+  function recurse(node) {
+      if (node) {
+          if (node.left) recurse(node.left);
+          if (node.right) recurse(node.right);
+          results.push(node.val);
+      }
+  }
+  recurse(root);
+  return results;
+};
+
+//Return to this one. Remember we don't need the depth, just to return in format like [[1], [2,3], [4,5,6]];
+var levelOrder = function(root) {
+  let results = [];
+  let queue = [root];
+  let level = 0;
+  while(queue.length) {
+      let currentSize = queue.length;
+      for (let i = 0; i < currentSize; i++) {
+          let pop = queue.shift();
+          results.push({
+              val: pop.val,
+              depth: level
+          });
+          if (pop.left !== null) queue.push(pop.left);
+          if (pop.right !== null) queue.push(pop.right);
+      }
+  }
+  return results;
+};

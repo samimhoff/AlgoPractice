4c386bb41335227b8aae4d763dbcc384dfe22463
Sam Imhoff <simhoffe@gmail.com>
2023-03-07 12:27:54 -0500

    Continued working on checknanddouble fast solution

--- checkIfNAndItsDoubleExist.js
+++ checkIfNAndItsDoubleExist.js
@@ -13,34 +13,54 @@ var checkIfExist = function(arr) {
   if (sortedArr[0] < 0) {
       let seeking = true;
       let i = 0;
-      let j = arr.length;
-      let half;
+      let j = arr.length-1;
       let n = 0;
       while (seeking && n < 20) {
-          half = Math.floor((j-i)/2);
+          half = Math.floor((j+i)/2);
+          console.log('i, half, j: ', i, half, j);
+          console.log('sortedarr[i], ...: ', sortedArr[i], sortedArr[half], sortedArr[j]);
           if (j-i < 3) {
-              half
+              let x = i;
+              while(sortedArr[x] > -1) {
+                  x--;
+              }
+              firstNegative = x;
+              seeking = false;
           }
-          if (sortedArr[half] < 0) {
+          if (sortedArr[half] <= 0) {
               i = half;
           } else {
               j = half;
           }
       }
   }
-  console.log('half: ', firstNegative, arr[firstNegative]);
+  console.log('half: ', firstNegative, sortedArr[firstNegative]);
+  if (checkHalf(sortedArr.slice(0, firstNegative), true) === true) return true;
+  if (checkHalf(sortedArr.slice(firstNegative, sortedArr.length-1), false) === true) return true;
+  return false;
+};
 
-
-  console.log('sortedArr: ', sortedArr);
+const checkHalf = (arr, isPositive) => {
   let i = 0, j = arr.length - 1;
-  while (i < j) {
-      if (arr[i] * 2 === arr[j]) return true;
-      if (arr[i] * 2 < arr[j]) {
-          i++;
-      } else {
-          j--;
+  if (isPositive) {
+      while (i < j) {
+          if (arr[i] * 2 === arr[j]) return true;
+          if (arr[i] * 2 < arr[j]) {
+              i++;
+          } else {
+              j--;
+          }
+      }
+  } else { // determine how to check negative.
+      while (i < j) {
+          if (arr[i] * 2 === arr[j]) return true;
+          if (arr[i] * 2 > arr[j]) {
+              i++;
+          } else {
+              j--;
+          }
       }
   }
   return false;
-};
+}
 

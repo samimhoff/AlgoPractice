63d6f7eef0ce2a8f656c42183455b01ca121bed3
Sam Imhoff <simhoffe@gmail.com>
2023-03-07 16:20:13 -0500

    Added progress on wiggleSort

--- wiggleSort.js
+++ wiggleSort.js
@@ -1,11 +1,20 @@
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
- * https://leetcode.com/problems/wiggle-sort-ii/
  */
 var wiggleSort = function(nums) {
+  //solution needs slight tuning.
   let sorted = nums.sort((a, b) => a - b);
+  let half = Math.floor(sorted.length-1/2) - 1;
+  let j = half+1;
   console.log(sorted);
-  //pretty challenging problem! return to it.
-  //[0923456781]
+  for (let i = 1; i < half-1; i+=2) {
+      let prev = sorted[i];
+      sorted[i] = sorted[j];
+      sorted[j] = prev;
+      j+=2;
+  }
+  console.log(sorted);
+  return sorted;
+  //[5274163809]
 };
